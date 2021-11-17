35ed9775ea65a180a602c97482f1fdcf05d1777d
pradeep <pradeeppachigolla168@gmail.com>
2021-11-17 21:56:07 +0530

    added pradeep sai phani

--- testgit/hello.c
+++ testgit/hello.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	printf("Hello World!\n");
-	printf("Hello World!\n");
-	printf("Hello World!\n");
-	printf("Hello World!\n");
-	printf("Hello World!\n");
+	printf("Hello pradeep!\n");
+
+	printf("Hello sai!\n");
+	printf("Hello phani!\n");
 
 	
 	return 0;
